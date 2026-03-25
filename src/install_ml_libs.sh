61be2568550983aa4dfe7e8ba64cf507f056ee03
RK <corvus.rom@gmail.com>
2026-03-25 21:19:06 +0700

    attemt

--- src/install_ml_libs.sh
+++ src/install_ml_libs.sh
@@ -3,3 +3,6 @@ set -e
 # Используем pip3, так как в Dataproc по умолчанию Python 3
 sudo /usr/bin/pip3 install matplotlib pandas mlflow seaborn
 echo "Библиотеки успешно установлены."
+
+#sudo /opt/conda/bin/pip install --upgrade pip
+#sudo /opt/conda/bin/pip install mlflow scikit-learn pandas matplotlib seabornc
