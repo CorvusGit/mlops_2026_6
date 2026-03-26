f7cae2880a57b0666da44b2bbcbbdc3bc72a3960
RK <corvus.rom@gmail.com>
2026-03-26 22:32:11 +0700

    cluster inicial

--- src/install_ml_libs.sh
+++ src/install_ml_libs.sh
@@ -1,7 +1,14 @@
-# Остановить выполнение при любой ошибке
+#!/bin/bash
 set -e
-/usr/bin/python3 -m pip install --upgrade pip
-/usr/bin/python3 -m pip install mlflow seaborn
+# # Остановить выполнение при любой ошибке
+# /usr/bin/python3 -m pip install --upgrade pip
+# /usr/bin/python3 -m pip install mlflow seaborn
 
-#sudo /opt/conda/bin/pip install --upgrade pip
-#sudo /opt/conda/bin/pip install pandas mlflow matplotlib seaborn
+# Находим путь к conda, если /opt/conda вдруг не там
+CONDA_BIN="/opt/conda/bin/conda"
+PIP_BIN="/opt/conda/bin/pip"
+
+# Устанавливаем библиотеки через pip, встроенный в conda
+# Это гарантирует, что они попадут в базовое окружение (base)
+$PIP_BIN install --upgrade pip
+$PIP_BIN install mlflow seaborn
